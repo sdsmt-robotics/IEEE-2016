11a9cd3b63b0ae8fc03ac445e52cc8019cfd3c82
cobaltnova <benjamin.garcia@mines.sdsmt.edu>
2016-02-13 13:22:39 -0700

    Begin writing the functions that will govern navigation logic.

---
+++ framework/src/navigation.c
@@ -0,0 +1,41 @@
+/*
+* Description: This file will govern our navigation logic, and will handle
+* the speed of our wheels using the input from our sensors.
+*
+* Expected Inputs: Currently we have 3 IR sensors; left, right, and center.
+*
+* Outputs: We will command the speed of the stepper-motors for two wheels.
+*
+* Requirements: We have to be able to handle driving straight ahead, turning
+* left and right in place, and driving in reverse.  
+*/
+#include "navigation.h"
+
+
+/*
+* This function will make final decisions about how to move.
+*/
+void navigate()
+{
+    
+}
+
+/*
+* This function will update the current distances with the sensor input.
+*/
+void update (int sensor1, int sensor2, int sensor2)
+{
+    /*These variables are placeholders*/
+    int distanceLeft = sensorInputToDistance(sensor1);
+    int distanceRight = sensorInputToDistance(sensor2);
+    int distanceCenter = sensorInputToDistance(sensor3);
+}
+
+/*
+* This function will turn the value that the IR sensors provide into a usable
+* distance.
+*/
+int sensorInputToDistance(int input)
+{
+    /*TODO: determine what the numeric sensor output means as a distance*/
+}
