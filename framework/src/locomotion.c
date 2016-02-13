dc33cd25e4cea988a2165523921876268ee5d140
Notgnoshi <notgnoshi@gmail.com>
2016-02-13 13:33:27 -0700

    initial locomotion framework

--- framework/src/locomotion.c
+++ framework/src/locomotion.c
@@ -1,7 +1,27 @@
 #include "locomotion.h"
-#include "serial.h"
+#include "serial.h" //not necessarily needed for this.
+#include "robot_defines.h"
 
-void wheelSpeed( side wheel, int speed )
+
+void setWheelSpeed( int wheel, unsigned char speed, int serial_port )
 {
+    unsigned char flag = 0;
+    unsigned char rate = speed;
+
+    if ( wheel == RIGHT )
+    {
+        printf("Writing %d to right motor.\r", rate);
+        flag = RIGHT_MOTOR_FLAG;
+        write( serial_port, &flag, 1 );
+        write( serial_port, &rate, 1 );
+    } else if ( wheel == LEFT )
+    {
+        printf("Writing %d to left motor.\r", rate);
+        flag = LEFT_MOTOR_FLAG;
+        write( serial_port, &flag, 1 );
+        write( serial_port, &rate, 1 );
+    } else {
+        printf("Can't write to a wheel that isn't there.\n");
+    }
 
 }
