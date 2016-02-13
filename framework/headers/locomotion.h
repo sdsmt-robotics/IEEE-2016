88a13356f7ce799268be589269f8312fee7c8eaf
Notgnoshi <notgnoshi@gmail.com>
2016-02-13 12:43:33 -0700

    more working on initial framework

--- framework/headers/locomotion.h
+++ framework/headers/locomotion.h
@@ -1,1 +1,7 @@
 /*Arduino locomotion wrapper*/
+typedef side int;
+
+side LEFT = 1;
+side RIGHT = 0;
+
+void wheelSpeed( side wheel, int speed );
--- framework/src/main.c
+++ framework/src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main_loop.h"
 
 int main( int argc, char* argv[] )
 {
