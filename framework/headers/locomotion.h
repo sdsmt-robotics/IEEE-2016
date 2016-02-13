32f76afec404d4f50ec917e54fe5ba95c76f8219
Notgnoshi <notgnoshi@gmail.com>
2016-02-13 13:40:06 -0700

    added clarifying comment

--- framework/headers/locomotion.h
+++ framework/headers/locomotion.h
@@ -5,7 +5,9 @@
 
 #define RIGHT 1
 #define LEFT 0
-
+/*
+ * 127 is the center point. rate > 127 is forward. rate < 127 is reverse
+ */
 void setWheelSpeed( side wheel, unsigned char speed, int serial_port );
 
 #endif
