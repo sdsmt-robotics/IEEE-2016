ccc19cf3a54d5495c419c3151309b07e98eb7d54
Notgnoshi <notgnoshi@gmail.com>
2016-03-05 14:34:12 -0700

    hard coded the sensor functions used in navigation.c until we get actual sensors

---
+++ include/sensors.h
@@ -0,0 +1,12 @@
+#ifndef SENSORS_H
+#define SENSORS_H
+
+int Left_IR();
+int Right_IR();
+int Forward_IR();
+int Front_Right_IR();
+int Front_Left_IR();
+int Backward_IR();
+
+
+#endif
---
+++ src/sensors.c
@@ -0,0 +1,31 @@
+#include "../include/sensors.h"
+
+int Left_IR()
+{
+	return 0;
+}
+
+int Right_IR()
+{
+	return 0;
+}
+
+int Forward_IR()
+{
+	return 0;
+}
+
+int Front_Right_IR()
+{
+	return 0;
+}
+
+int Front_Left_IR()
+{
+	return 0;
+}
+
+int Backward_IR()
+{
+	return 0;
+}
