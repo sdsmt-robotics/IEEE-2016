3ce343a9c279f3247b3e7089de42357197d65995
Notgnoshi <notgnoshi@gmail.com>
2016-03-05 11:09:12 -0700

    driveWheelSteps() should work

--- src/locomotion.c
+++ src/locomotion.c
@@ -9,7 +9,7 @@
 
 #define printf LOG
 
-void setWheelSpeed( int wheel, unsigned char speed, int serial_port )
+/*void setWheelSpeed( int wheel, unsigned char speed, int serial_port )
 {
     unsigned char motor_flag = 0;
     unsigned char rate = speed;
@@ -32,15 +32,38 @@ void setWheelSpeed( int wheel, unsigned char speed, int serial_port )
         printf("Can't write to a wheel that isn't there.\n");
         printf("Hopefully you never see this line %d\n", result);
     }
-}
+}*/
 
-void driveWheelSteps( int wheel, int steps, float time, int serial_port )
+void driveWheelSteps( int wheel, int steps, int time, int serial_port ) //arduino will eventually expect milliseconds
 {
-    printf("Driving %d wheel %d steps in %f seconds\n", wheel, steps, time);
-    //TODO: implement this
+    printf("\tDriving %d wheel %d steps in %d seconds\n", wheel, steps, time);
+    int n = 0;
+    unsigned char motor_flag = 0;
+
+    if ( wheel == RIGHT )
+    {
+        printf("\tWriting to right motor.\r");
+        motor_flag = RIGHT_MOTOR_FLAG;
+        n = n + write( serial_port, &motor_flag, 1 );
+        n = n + write( serial_port, &steps, sizeof(steps) );
+        n = n + write( serial_port, &time, sizeof(time) );
+        printf("\t%d bytes written to wheel %d\r", n, motor_flag );
+    } else if ( wheel == LEFT )
+    {
+        printf("\tWriting to left motor.\r");
+        motor_flag = LEFT_MOTOR_FLAG;
+        n = n + write( serial_port, &motor_flag, 1 );
+        n = n + write( serial_port, &steps, sizeof(steps) );
+        n = n + write( serial_port, &time, sizeof(time) );
+        printf("\t%d bytes written to wheel %d\r", n, motor_flag );
+    } else
+    {
+        printf("Can't write to a wheel that isn't there.\n");
+        printf("Hopefully you never see this line %d\n", n);
+    }
 }
 
-void turn( int serial_port, int angle, float time )
+void turn( int serial_port, int angle, int time )
 {
     double arc_length;
     int steps;
@@ -55,20 +78,29 @@ void turn( int serial_port, int angle, float time )
 
      steps = round( STEPS_PER_MM * arc_length );
 
+     printf("Turning %d degrees in %d seconds, which corresponds to an arc length of %f and %d number of steps per wheel\r", angle, time, arc_length, steps );
+
      driveWheelSteps( RIGHT, -steps, time, serial_port );
      driveWheelSteps( LEFT, steps, time, serial_port );
 }
 
-void drive( int serial_port, float distance, float time )
+void drive( int serial_port, float distance, int time )
 {
     // Drives the robot forward at `speed` for `distance`, in cm.
 
     int steps;
 
-    steps = STEPS_PER_CM * distance;
+    steps = round( STEPS_PER_CM * distance );
+    printf("Driving straight %d steps\r", steps );
 
     driveWheelSteps( RIGHT, steps, time, serial_port );
     driveWheelSteps( LEFT, steps, time, serial_port );
+}
+
+void stop( int serial_port )
+{
+    driveWheelSteps( RIGHT, 0, 0, serial_port );
+    driveWheelSteps( LEFT, 0, 0, serial_port );
 }
 
 void close_claws( int serial_port )
