163f7e0ad80e181f61b41997aaf5020a416077eb
Notgnoshi <notgnoshi@gmail.com>
2016-03-06 15:38:16 -0700

    trying to close the file before we open it

--- src/test.c
+++ src/test.c
@@ -13,12 +13,13 @@
 
 int main( int argc, char* argv[] )
 {
+    int closed = close( 4 );
+    printf("closed = %d\n", closed);
     int serial_file = sys_init();
     unsigned char flag = RIGHT_MOTOR_STEPS_FLAG;
     int steps = 400;
     int seconds = 2;
-    int closed = close( 4 );
-    printf("closed = %d\n", closed);
+
     printf("\nMoving %d steps in %d seconds.\n", steps, seconds);
     int n = write(serial_file, &flag, 1);
     n = n + write(serial_file, &steps, sizeof(steps));
