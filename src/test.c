51b7c30fc15e64f70ee7bdd9bd994e815e9459e3
Notgnoshi <notgnoshi@gmail.com>
2016-03-08 13:54:32 -0700

    slowly narrowing down what the issue is

--- src/test.c
+++ src/test.c
@@ -14,7 +14,6 @@
 int main( int argc, char* argv[] )
 {
     int serial_file = sys_init();
-    char buffer[512] = "";
     /* For some reason this is no longer working. It ran on the very first try and never since.
      * What I think is happening is that serial_init() is no opening the correct file, because
      * in the joystick test the serial_file is 4, and here's 3. I'm not sure it matter though,
@@ -30,7 +29,6 @@ int main( int argc, char* argv[] )
      */
     while (1)
     {
-        sleep(1);
         unsigned char flag = RIGHT_MOTOR_STEPS_FLAG;
         int steps = 400;
         int seconds = 2;
@@ -40,15 +38,7 @@ int main( int argc, char* argv[] )
         n = n + write(serial_file, &steps, sizeof(steps));
         n = n + write(serial_file, &seconds, sizeof(seconds));
         printf("Wrote %d bytes.\n", n);
-/*
-        int m = read(serial_file, &buffer, sizeof(buffer));
-        if(m > 0)
-        {
-            buffer[m] = '\0';
-            printf("\n-%s", buffer);
-            fflush(stdout);
-        }
-*/
+        break;
     }
     
 
