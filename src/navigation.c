feabaa87ef7ea74abb870a422d1b64ff7d245b13
Notgnoshi <notgnoshi@gmail.com>
2016-03-11 19:48:28 -0700

    locomotion things

--- src/navigation.c
+++ src/navigation.c
@@ -23,7 +23,7 @@ void start_to_cp( int serial_port )
 {
 /* ********** BEGIN FORWARD TO EDGE OF WALL ********** */
     //wall follow until left sensor reading goes high
-    setWheelSpeed( BOTH, 90, serial_port );
+    setWheelSpeed( BOTH, 127, serial_port );
     printf("outside loop.\n");
     while ( left_sensor() < SIX_INCHES )
     {
